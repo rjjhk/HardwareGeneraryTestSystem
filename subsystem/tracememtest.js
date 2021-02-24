2b3940ada8e756bf2b165d7146f02835ba3b90af
yingsan <yingsanrjjhk@gmail.com>
2021-02-24 08:46:54 +0800

    -202102240850

--- subsystem/tracememtest.js
+++ subsystem/tracememtest.js
@@ -7,7 +7,7 @@ exports.handOverTraceMemTest = () => {
     fsP.readFile('./allconfig/tracememtestconfig.json',{encoding:'utf8'}).then(res => {
         config = JSON.parse(res)
     }).then(() => {
-        // test
+        // start test
         getADBdevices()
     }).catch(err =>{
         log(err)
@@ -29,10 +29,87 @@ let getADBdevices = () => {
                 devices.push({adbID:tempDeviceID})
             }
         }
+        // 判断是否没有设备
+        if(devices.length == 0){
+            log("ERROR:have not detect any adb device")
+            return
+        }
         // 启动定时器获取mem
         let getMEMTimeIntervalID = setInterval(()=>{
-            
-        },config.getMemIntervalTimeM)
-        console.log(devices)
+            for(let device of devices){
+                getUptime(device.adbID,getMEMTimeIntervalID)
+            }
+        },config.getMemIntervalTimeM * 1000)
+        // 立即触发
+        for(let device of devices){
+            getUptime(device.adbID,getMEMTimeIntervalID)
+        }
+    })
+}
+
+let getUptime = (adbID,intervalID) => {
+    let ls = spawn('./tool/adb/adb.exe',["-s",adbID,"shell","\"uptime\""])
+    ls.stdout.on("data",(data)=>{
+        let uptimeString = iconv.decode(data,'gbk')
+        // 检测adb设备是否异常
+        if(uptimeString.match(/no device/i)){
+            clearInterval(intervalID)
+            log(uptimeString)
+        }
+        // 转换成时间
+        let timeS = 0
+        if(uptimeString.match(/\d+\s*day/i)){
+            timeS += Number(uptimeString.match(/\d+\s*day/i)[0].match(/\d+/)[0]) * 24 * 60 * 60
+        }
+        if(uptimeString.match(/\d+\s*min/i)){
+            timeS += Number(uptimeString.match(/\d+\s*min/i)[0].match(/\d+/)[0]) * 60 
+        }else{
+            timeS += Number(uptimeString.slice(8).match(/\d+:/i)[0].slice(0,-1)) * 60 * 60
+            timeS += Number(uptimeString.slice(8).match(/:\d+/i)[0].slice(1)) * 60
+        }
+        // 运行时间写入维护对象
+        for(let device of devices){
+            if(device.adbID == adbID){
+                if(device.hasOwnProperty("upTime")){
+                    // 判断运行时间是否异常
+                    if(device.upTime > timeS){
+                        // 异常退出
+                        sentMessageToWeChat("Device which adbID is " + adbID + " error occur ")
+                        clearInterval(intervalID)
+                    }else{
+                        device.upTime = timeS
+                    }
+                }else{
+                    device.upTime = timeS
+                }
+                break
+            }
+        }
+        // 转获取mem信息
+        getOrcaMEM(adbID,intervalID)
+    })
+}
+
+let getOrcaMEM = (adbID,intervalID) => {
+    let ls = spawn('./tool/adb/adb.exe',["-s",adbID,"shell","cat /proc/meminfo"])
+    ls.stdout.on("data",(data) => {
+        let orcaMEMString = iconv.decode(data,'gbk')
+        if(orcaMEMString.match(/memfree[\s\S]*?\d+\skB/i)){
+            // free写入维护对象
+            for(let device of devices){
+                if(device.adbID == adbID){
+                    if(device.hasOwnProperty("orcaMemFrees")){
+                        device.orcaMemFrees.push({date:})
+                    }else{
+                        device.upTime = timeS
+                    }
+                    break
+                }
+            }
+        }
+        console.log(orcaMEMString)
+    })
+    ls.on("close",() => {
+        clearInterval(intervalID)
     })
 }
